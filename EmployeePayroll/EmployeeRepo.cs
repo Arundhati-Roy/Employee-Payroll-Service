5c585775673e9686527d232ae337ef5504f6a2bc
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-11-03 09:17:28 +0530

    Update Query

--- EmployeePayroll/EmployeeRepo.cs
+++ EmployeePayroll/EmployeeRepo.cs
@@ -16,61 +16,26 @@ namespace Emp_wage_prob
                 Employee dataSet = new Employee();
                 using (this.sqlConnection)
                 {
-                    string query = @"SELECT * from employee";
+                    string query = @"UPDATE employee_payroll set basicPay=30000 
+                                    where id in (select id from employee_payroll where name='AR')";
 
                     //define sql object
                     SqlCommand cmd = new SqlCommand(query, this.sqlConnection);
 
                     this.sqlConnection.Open();
 
-                    SqlDataReader dr = cmd.ExecuteReader();
+                    //SqlDataReader dr = cmd.ExecuteReader();
 
-                    //check if there are records
-                    if (dr.HasRows)
+                    int rows = cmd.ExecuteNonQuery();
+                    this.sqlConnection.Close();
+                    if (rows > 0)
                     {
-                        while (dr.Read())
-                        {
-                            dataSet.empId = dr.GetInt32(0);
-                            dataSet.compId = dr.GetString(1);
-                            dataSet.empName = dr.GetString(2);
-                            try
-                            {
-                                dataSet.gender = dr.GetString(3);
-                            }
-                            catch
-                            {
-                                dataSet.gender = "";
-                            }
-                            try
-                            {
-                                dataSet.phNo = dr.GetString(4);
-                            }
-                            catch
-                            {
-                                dataSet.phNo = "";
-                            }
-                            try
-                            {
-                                dataSet.addr = dr.GetString(5);
-                            }
-                            catch
-                            {
-                                dataSet.addr = "";
-                            }
-                            /*if (dataReader["phoneNo"] != DBNull.Value)
-                                employee.PhoneNumber = dataReader.GetDecimal(4).ToString();*/
-                            //display retrieved record
-                            Console.WriteLine("{0},{1},{2},{3},{4},{5}", dataSet.empId, dataSet.empName, dataSet.compId, dataSet.gender, dataSet.phNo, dataSet.addr);
-                            Console.WriteLine("\n");
-                        }
+                        Console.WriteLine(rows + " row(s) affected");
                     }
                     else
                     {
-                        Console.WriteLine("No data found");
+                        Console.WriteLine("Please check your query");
                     }
-                    dr.Close();
-                    this.sqlConnection.Close();
-                    //return dataSet;
                 }
             }
             catch (Exception e)
