3f0b256dd6da91c9ab779758bdb2e98fdd712d29
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-11-03 11:32:33 +0530

    Retrieving Parameterised Query

--- EmployeePayroll/EmployeeRepo.cs
+++ EmployeePayroll/EmployeeRepo.cs
@@ -13,55 +13,53 @@ namespace Emp_wage_prob
         {
             try
             {
-                Employee dataSet = new Employee();
+                Employee emp = new Employee();
+                Department dept = new Department();
+                EmpDept empDept = new EmpDept();
+                Payroll payroll = new Payroll();
                 using (this.sqlConnection)
                 {
-                    string query = @"SELECT * from employee";
+                    string query = @"select * from employee e,department d,payroll p,empDept ed
+                                     where ed.deptId=d.deptId and e.empId=p.empId and ed.empId=e.empId
+                                     and e.name=@name";
+                    SqlParameter nameParam = new SqlParameter("@name", System.Data.SqlDbType.VarChar, 0);
+                    nameParam.Value = "PR";
 
                     //define sql object
                     SqlCommand cmd = new SqlCommand(query, this.sqlConnection);
+                    cmd.Parameters.Add(nameParam);
 
                     this.sqlConnection.Open();
 
                     SqlDataReader dr = cmd.ExecuteReader();
 
-                    //check if there are records
                     if (dr.HasRows)
                     {
                         while (dr.Read())
                         {
-                            dataSet.empId = dr.GetInt32(0);
-                            dataSet.compId = dr.GetString(1);
-                            dataSet.empName = dr.GetString(2);
-                            try
-                            {
-                                dataSet.gender = dr.GetString(3);
-                            }
-                            catch
-                            {
-                                dataSet.gender = "";
-                            }
-                            try
-                            {
-                                dataSet.phNo = dr.GetString(4);
-                            }
-                            catch
-                            {
-                                dataSet.phNo = "";
-                            }
-                            try
-                            {
-                                dataSet.addr = dr.GetString(5);
-                            }
-                            catch
-                            {
-                                dataSet.addr = "";
-                            }
-                            /*if (dataReader["phoneNo"] != DBNull.Value)
-                                employee.PhoneNumber = dataReader.GetDecimal(4).ToString();*/
-                            //display retrieved record
-                            Console.WriteLine("{0},{1},{2},{3},{4},{5}", dataSet.empId, dataSet.empName, dataSet.compId, dataSet.gender, dataSet.phNo, dataSet.addr);
+                            emp.empId = dr.GetInt32(0);
+                            emp.compId = dr.GetString(1);
+                            emp.empName = dr.GetString(2);
+                            if (dr["gender"] != DBNull.Value)
+                                emp.gender = dr.GetString(3);
+                            if (dr["empPhone"] != DBNull.Value)
+                                emp.phNo = dr.GetString(4);
+                            if (dr["addr"] != DBNull.Value)
+                                emp.addr = dr.GetString(5);
+                            dept.deptId = dr.GetString(6);
+                            dept.deptName = dr.GetString(7);
+                            payroll.startDate = dr.GetDateTime(10);
+                            payroll.basicPay = Convert.ToDouble(dr.GetDecimal(11));
+                            payroll.deductions = Convert.ToDouble(dr.GetDecimal(12));
+                            payroll.tax = Convert.ToDouble(dr.GetDecimal(13));
+                            payroll.incomeTax = Convert.ToDouble(dr.GetDecimal(14));
+                            payroll.NetPay = Convert.ToDouble(dr.GetDecimal(15));
+
+
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}", emp.empId, emp.empName, emp.compId, emp.gender,
+                                emp.phNo, emp.addr,dept.deptId,dept.deptName,payroll.startDate, payroll.basicPay,payroll.deductions,payroll.tax,payroll.incomeTax,payroll.NetPay);
                             Console.WriteLine("\n");
+
                         }
                     }
                     else
@@ -70,7 +68,6 @@ namespace Emp_wage_prob
                     }
                     dr.Close();
                     this.sqlConnection.Close();
-                    //return dataSet;
                 }
             }
             catch (Exception e)
