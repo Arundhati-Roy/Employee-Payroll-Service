a6da0ef26607cbcb30fb2181cca36b6a77d8681d
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-11-03 08:53:30 +0530

    first commit

---
+++ EmployeePayroll/EmployeeRepo.cs
@@ -0,0 +1,87 @@
+﻿using EmployeePayroll;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Emp_wage_prob
+{
+    public class EmpRepo
+    {
+        public static string connectString = @"Data Source=(LocalDb)\RoyDB;Initial Catalog=tsetDB;Integrated Security=True";
+        SqlConnection sqlConnection = new SqlConnection(connectString);
+        public void GetAllEmployee()
+        {
+            try
+            {
+                Employee dataSet = new Employee();
+                using (this.sqlConnection)
+                {
+                    string query = @"SELECT * from employee";
+
+                    //define sql object
+                    SqlCommand cmd = new SqlCommand(query, this.sqlConnection);
+
+                    this.sqlConnection.Open();
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    //check if there are records
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            dataSet.empId = dr.GetInt32(0);
+                            dataSet.compId = dr.GetString(1);
+                            dataSet.empName = dr.GetString(2);
+                            try
+                            {
+                                dataSet.gender = dr.GetString(3);
+                            }
+                            catch
+                            {
+                                dataSet.gender = "";
+                            }
+                            try
+                            {
+                                dataSet.phNo = dr.GetString(4);
+                            }
+                            catch
+                            {
+                                dataSet.phNo = "";
+                            }
+                            try
+                            {
+                                dataSet.addr = dr.GetString(5);
+                            }
+                            catch
+                            {
+                                dataSet.addr = "";
+                            }
+                            /*if (dataReader["phoneNo"] != DBNull.Value)
+                                employee.PhoneNumber = dataReader.GetDecimal(4).ToString();*/
+                            //display retrieved record
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5}", dataSet.empId, dataSet.empName, dataSet.compId, dataSet.gender, dataSet.phNo, dataSet.addr);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                    dr.Close();
+                    this.sqlConnection.Close();
+                    //return dataSet;
+                }
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("Null data found");
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.sqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ EmployeePayroll/Program.cs
@@ -0,0 +1,34 @@
+﻿using Emp_wage_prob;
+using System;
+
+namespace EmployeePayroll
+{
+    class Employee
+    {
+        public int empId { get; set; }
+        public string empName { get; set; }
+        public string phNo { get; set; }
+        public string addr { get; set; }
+        public string compId { get; set; }
+        public string gender { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll");
+            EmpRepo repo = new EmpRepo();
+            Employee emp = new Employee();
+
+            /*emp.empId = 1;
+            emp.compId = "C1";
+            emp.empName = "AR";
+            emp.gender = "F";
+            emp.phNo = "568798089";
+            emp.addr = "Mumbai";
+*/
+            repo.GetAllEmployee();
+        }
+    }
+}
